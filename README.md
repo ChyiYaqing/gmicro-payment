a57274c5133073a25951aa8e70a42e34f6034fc6
ChyiYaqing <chyiyaqing@gmail.com>
2024-12-19 11:08:50 +0800

    add client validate

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 运行Payment服务
 
 ```bash
-➜ SQLITE_DB=data/sqlite.db APPLICATION_PORT=8081 ENV=development go run cmd/main.go
-2024/12/13 19:26:06 starting order service on port 8081 ...
+➜ SQLITE_DB=data/sqlite.db APPLICATION_PORT=8180 ENV=development go run cmd/main.go
+2024/12/13 19:26:06 starting order service on port 8180 ...
 ```
--- internal/adapters/db/grpc/grpc.go
+++ internal/adapters/db/grpc/grpc.go
@@ -7,12 +7,27 @@ import (
 	"github.com/chyiyaqing/gmicro-payment/internal/application/core/domain"
 	"github.com/chyiyaqing/gmicro-proto/golang/payment"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (a *Adapter) Create(ctx context.Context, request *payment.CreatePaymentRequest) (*payment.CreatePaymentResponse, error) {
 	log.WithContext(ctx).Info("Creating payment...")
+	var validationErrors []*errdetails.BadRequest_FieldViolation
+	if request.UserId < 1 {
+		validationErrors = append(validationErrors, &errdetails.BadRequest_FieldViolation{
+			Field:       "user_id",
+			Description: "user id canot be less than 1",
+		})
+	}
+	if len(validationErrors) > 0 {
+		stat := status.New(400, "invalid order request")
+		badRequest := &errdetails.BadRequest{}
+		badRequest.FieldViolations = validationErrors
+		s, _ := stat.WithDetails(badRequest)
+		return nil, s.Err()
+	}
 	newPayment := domain.NewPayment(request.UserId, request.OrderId, request.TotalPrice)
 	result, err := a.api.Charge(ctx, newPayment)
 	if err != nil {
